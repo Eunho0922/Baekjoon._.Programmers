6f09de712683a48cf25608dcfc0157cb603cd32f
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-17 10:48:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++ ) {
+        for (int j = i; j < N; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
