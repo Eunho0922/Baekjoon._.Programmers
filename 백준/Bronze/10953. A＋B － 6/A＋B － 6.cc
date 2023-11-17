beead07d1b03b557d95627941546a5052b726da0
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-17 17:00:54 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, a, b, c;
+    char comma;
+    
+    cin >> T;
+    
+    for (int i = 0; i < T; i++) {
+        cin >> a >> comma >> b;
+        c = a + b;
+        cout << c << endl;
+    }
+    
+}
