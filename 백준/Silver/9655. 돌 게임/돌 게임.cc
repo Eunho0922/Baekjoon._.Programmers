9948916f29671c5935562f77813551e8b3964262
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-21 22:47:11 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+    if (N % 2==0) {
+        cout << "CY";
+    } else {
+        cout << "SK";
+
+    }
+}
