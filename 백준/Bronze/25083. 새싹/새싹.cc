796a611d026f35660b2dd90701f91e7527740a4b
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-27 22:18:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    
+    cout << "         ,r\'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r\'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+    
+}
