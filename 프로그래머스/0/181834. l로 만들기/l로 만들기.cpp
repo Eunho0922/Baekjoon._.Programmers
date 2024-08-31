04ef89ab51c1c2525b16897e7e7ccedb0f0c966c
이은호 <127753071+Eunho0922@users.noreply.github.com>
2024-09-01 00:48:30 +0900

    [level 0] Title: l로 만들기, Time: 0.54 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for (int i = 0; i < myString.size(); i++) {
+        if (myString[i] < 'l')
+            myString[i] = 'l';
+    }
+    answer = myString;
+    return answer;
+}
