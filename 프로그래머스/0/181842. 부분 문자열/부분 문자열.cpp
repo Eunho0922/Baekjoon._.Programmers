34e0c9f2279357fd7ec9777a34b2a1cbc8da39bc
이은호 <127753071+Eunho0922@users.noreply.github.com>
2024-09-01 00:01:40 +0900

    [level 0] Title: 부분 문자열, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer = 0;
+    if (str2.find(str1) == string::npos) {
+        return 0;
+    }
+    return 1;
+}
