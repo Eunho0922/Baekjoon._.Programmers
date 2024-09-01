55ecff70a9f1dd44c81ff3b13783b5283ef87333
이은호 <127753071+Eunho0922@users.noreply.github.com>
2024-09-01 11:02:05 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, string alp) {
+    string answer = "";
+    for (int i = 0; i < my_string.size(); ++i) {
+        if (my_string[i] == alp[0])
+            answer += my_string[i] - 32;
+        else
+            answer += my_string[i];
+    }
+    return answer;
+}
