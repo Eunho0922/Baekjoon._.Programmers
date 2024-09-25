75820ea5255f41f4986cb0c17e411ffc0cc26f52
이은호 <127753071+Eunho0922@users.noreply.github.com>
2024-09-25 16:47:11 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include<queue>
+using namespace std;
+ 
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    priority_queue <int> pq;
+    queue<pair<int, int>> q;
+    for (int i = 0; i < priorities.size(); i++) {
+        pq.push(priorities[i]);
+        q.push(make_pair(priorities[i], i));
+    }
+ 
+    while (true) {
+        int a = q.front().first;
+        int b = q.front().second;
+        q.pop();
+ 
+        if (a == pq.top()) {
+            pq.pop();
+            answer++;
+            if (b == location) break;
+        }
+        else {
+            q.push(make_pair(a, b));
+        }
+ 
+    }
+    return answer;
+}
+ 
